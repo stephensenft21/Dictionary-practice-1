12711b9b42acc3f92cf03404ab4f59f77ab0d49a
stephensenft21 <stephensenft21@gmail.com>
2019-11-16 12:43:38 -0600

    finished working on adding dictionaries

--- DictionaryPractice/Program.cs
+++ DictionaryPractice/Program.cs
@@ -44,6 +44,90 @@ foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
 
 
 }
+
+// Make a new list
+List<Dictionary<string, string>> dictionaryOfExcitedWords = new List<Dictionary<string, string>>();
+List<Dictionary<string, string>> dictionaryOfBadWords = new List<Dictionary<string, string>>();
+List<Dictionary<string, string>> dictionaryOfFunWords = new List<Dictionary<string, string>>();
+List<Dictionary<string, string>> dictionaryOfNewWords = new List<Dictionary<string, string>>();
+
+
+
+
+/*
+    You want to track the following about each word:
+        word, definition, part of speech, example sentence
+
+    Example of one dictionary in the list:
+    {
+        
+        "word": "excited",
+        "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+        "part of speech": "adjective",
+        "example sentence": "I am excited to learn C#!"
+    }
+*/
+
+// Create dictionary to represent a few words
+Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+Dictionary<string, string> funWord = new Dictionary<string, string>();
+Dictionary<string, string> badWord = new Dictionary<string, string>();
+Dictionary<string, string> newWord = new Dictionary<string, string>();
+
+// Add each of the 4 bits of data about the word to the Dictionary
+excitedWord.Add("Word", "Yahoo!");
+excitedWord.Add("Definition", "a rude, noisy, or violent person!");
+excitedWord.Add("Part of speech", "noun");
+excitedWord.Add("Example sentence", "that dude was such a yahoo!");
+// Add each of the 4 bits of data about the word to the Dictionary
+funWord.Add("Word","Bitchin");
+funWord.Add("Definition","remarkably good or cool");
+funWord.Add("Part of speech","adjective");
+funWord.Add("Example sentence","that store was bitchin");
+// Add each of the 4 bits of data about the word to the Dictionary
+badWord.Add("Word","Butt");
+badWord.Add("Definition","the thicker end of something, especially a tool or a weapon");
+badWord.Add("Part of speech","noun");
+badWord.Add("Example sentence","Thats a nice butt");
+
+newWord.Add("Word","New");
+newWord.Add("Defintion","Not existing before; made, introduced, or discovered recently or now for the first time.");
+newWord.Add("Part of speech","adjective");
+newWord.Add("Example sentence","She helped Carmen into the new dress.");
+
+
+// Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfExcitedWords.Add(excitedWord);
+            dictionaryOfBadWords.Add(badWord);
+            dictionaryOfFunWords.Add(funWord);
+            dictionaryOfNewWords.Add(newWord);
+            
+
+
+
+// create another Dictionary and add that to the list
+
+
+/*
+    Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+    Example output for one word in the list of dictionaries:
+        word: excited
+        definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+        part of speech: adjective
+        example sentence: I am excited to learn C#!
+*/
+
+// Iterate the List of Dictionaries
+foreach (var wordData in dictionaryOfWords )
+{
+
+    // Iterate the KeyValuePairs of the Dictionary
+    foreach (var word in wordData)
+    {
+        Console.WriteLine($"{word.Key}: {word.Value}");
+    }
+}
         }
     }
 }
